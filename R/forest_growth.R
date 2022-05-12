f4738400036bfbbd5a245a0ace8b8688d65a0319
Steven Cognac <scognac@bren.ucsb.edu>
2022-05-12 13:59:46 -0700

    update forest_growth

--- R/forest_growth.R
+++ R/forest_growth.R
@@ -1,17 +1,18 @@
 #' Forest growth model
+#' @param time time since start
 #' @param C size of the forest
-#' @param parms - as list with three values, r, K, g
-#' @param K carrying capacity (units: carbon)
+#' @param parms - as list with three values; r, K, g, thresh
 #' @param r early exponential growth rate
+#' @param K carrying capacity (kgC)
 #' @param g linear growth rate once canopy closure has been reached
-#' @param thresh threshold canopy closure
-#' @return forest growth rate
+#' @param thresh threshold canopy closure (kgC)
+#' @return derivative of forest growth over time
 #'
-forest_growth = function(C, K, r, g, thresh) {
+forest_growth = function(Time, C, parms) {
   
   # compute forest growth
-  
-  dC = ifelse(C < thresh, parms$r*C, parms$g*(1-(C/parms$K)))
+  dC = ifelse(C < parms$thresh, parms$r*C, parms$g)
+  dC = ifelse(C >= parms$thresh, parms$g*(1-(C/parms$K)))
   
   return(list(dC))
 }
